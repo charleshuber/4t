7322ecd81a63b4714a60a0e9f7dfa5bbe0718485
charles <charles.huber@hotmail.fr>
2017-03-20 17:15:37 +0100

    First commit

---
+++ business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/db/entity/UserDbo.java
@@ -0,0 +1,41 @@
+package fr.metz.surfthevoid.tttt.rest.resources.db.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USER")
+public class UserDbo extends GenericDbo {
+	
+	private String email;
+	private String password;
+	private String lastName;
+	private String firstName;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+}
---
+++ business-rs-definition/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/Resource.java
@@ -0,0 +1,14 @@
+package fr.metz.surfthevoid.tttt.rest.resources;
+
+public abstract class Resource {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}	
+}

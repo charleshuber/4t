d26eec104994d10e5ff09a279c62e524d3c8a0fa
charleshuber <charles.huber@hotmail.fr>
2017-05-25 08:33:02 +0200

    env var JAVA

--- docker/local/wait-mysql.sh
+++ docker/local/wait-mysql.sh
@@ -10,7 +10,6 @@ cmd="$@"
 
 >&2 echo " Test connection on $host:$port"
 
-
 until nc -z -v -w30 "$host" "$port"
 do
   >&2 echo "Waiting for database connection..."
@@ -18,4 +17,9 @@ do
 done
 
 >&2 echo "Mysql is now ready to accept connection, let's go to start tomcat container with command exec $cmd"
+
+export JAVA_OPTS="$JAVA_OPTS -DMYSQL_DB_HOST=$MYSQL_DB_HOST -DMYSQL_DB_PORT=$MYSQL_DB_PORT -DMYSQL_DB_NAME=$MYSQL_DB_NAME -DMYSQL_USER=$MYSQL_USER -DMYSQL_PASSWORD=$MYSQL_PASSWORD"
+
+>&2 echo "$JAVA_OPTS"
+
 exec $cmd

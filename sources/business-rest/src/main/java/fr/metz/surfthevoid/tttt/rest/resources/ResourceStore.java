accdbd01d7b40ec6807bc2c20de7eee39e0196a7
charleshuber <charles.huber@hotmail.fr>
2017-06-08 21:46:11 +0200

    commentaires

--- sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/ResourceStore.java
+++ sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/ResourceStore.java
@@ -5,60 +5,152 @@ import javax.transaction.Transactional.TxType;
 
 import fr.metz.surfthevoid.tttt.rest.db.entity.GenericDbo;
 import fr.metz.surfthevoid.tttt.rest.db.repo.GenericDao;
-import fr.metz.surfthevoid.tttt.rest.resources.ValidationException.Errors;
-import fr.metz.surfthevoid.tttt.rest.resources.ValidationException.Type;
 
-
+/**
+ * Classe abstraite générique de services pour la gestion des ressources
+ * Elle regroupe l'implémentation des quatres méthodes principales dont héritent les classes filles
+ * @author charles
+ *
+ * @param <R>
+ * @param <T>
+ */
+// Toutes les méthodes publiques appelées depuis un autre composant sont executées
+// dans une nouvelle transaction en base de donnée
 @Transactional(TxType.REQUIRES_NEW)
 public abstract class ResourceStore<R extends Resource, T extends GenericDbo> {
 	
-	protected abstract GenericDao<T> getDao(); 
-	protected abstract Validator<R,T> getValidator();
-	
-	public R create(R res) throws ValidationException{
+	/**
+	 * Méthode générique de création d'une ressource
+	 * @param res
+	 * @return
+	 * @throws ValidationException
+	 */
+	public R create(R res) throws ValidationException {
+		// On valide la ressource et on la transforme vers le modèle de base de donnée
 		T dbo = validateAndTransform(res, Operation.CREATE);
+		//on l'enregistre en base avec le dao de la ressource
 		getDao().create(dbo);
+		// on retransforme le résultat de l'enregistrement vers le modèle de transfert avant de le renvoyer
+		// Il peut contenir des données supplémentaire par rapport au modèle d'origine (eq. l'identifiant)
 		return extract(dbo);
 	}
 	
-	public R read(Long id) throws ValidationException{
+	/**
+	 * Méthode générique de lecture d'une ressource
+	 * @param res
+	 * @return
+	 * @throws ValidationException
+	 */
+	public R read(Long id) throws ValidationException {
+		// récupère de validateur de la ressource pour valider l'identifiant
 		getValidator().validateId(id);
+		// si l'identifiant est valide on renvoie la ressource,
+		// après l'avoir transformer vers le modèle de transfert
 		T dboToRead = getDao().read(id);
-		if(dboToRead == null){
-			throw new ValidationException(Type.NO_CONTENT, new Errors());
-		}
+		// on retransforme le résultat de l'enregistrement vers le modèle de transfert
 		return extract(dboToRead);
 	}
 	
+	/**
+	 * Méthode générique de mise à jour d'une ressource
+	 * @param res
+	 * @return
+	 * @throws ValidationException
+	 */
 	public R update(R res) throws ValidationException{
+		// On valide la ressource et on la transforme vers le modèle de base de donnée
 		T dbo = validateAndTransform(res, Operation.UDPDATE);
+		// on l'a met à jour en base avec le dao de la ressource avant de le renvoyer
 		T mergedDbo = getDao().update(dbo);
 		return extract(mergedDbo);
 	}
 
+	/**
+	 * Méthode générique de suppression d'une ressource
+	 * @param res
+	 * @return
+	 * @throws ValidationException
+	 */
 	public R delete(Long id) throws ValidationException{
+		// récupère de validateur de la ressource pour valider l'identifiant
 		getValidator().validateId(id);
+		// si l'identifiant est valide on supprime la ressource,
 		T dboToDelete = getDao().read(id);
-		if(dboToDelete == null){
-			throw new ValidationException(Type.NO_CONTENT, new Errors());
-		}
 		getDao().delete(dboToDelete);
+		// On renvoie la ressource supprimée après l'avoir transformée vers le modèle de transfert
 		return extract(dboToDelete);
 	}
 	
+	/**
+	 * Méthode générique pour valider et transformer une ressource vers son modèle de base de donnée
+	 * @param res
+	 * @return
+	 * @throws ValidationException
+	 */
 	protected T validateAndTransform(R res, Operation op) throws ValidationException{
+		// avant la validation on remplie la ressource avec les valeur par défaut
+		// et on nettoie les valeurs si besoin
+		// ces 2 méthodes, fillWithDefault et clean peuvent être ré-implémentée par les classes filles
 		if(res != null){
 			res = fillWithDefault(res, op);
 			res = clean(res, op);	
 		}
+		// On valide la ressource avec le validateur de la ressource
 		getValidator().validate(res, op);
+		// On effectue les actions successives à la validation (la méthode peut être ré-implémentées par les classes filles)
 		res = postValidationActions(res, op);
+		// On tranforme la ressource vers son modèle de base de donnée
 		return transform(res);
 	}
 	
-	protected R postValidationActions(R res, Operation op) { return res; };
-	protected R fillWithDefault(R res, Operation op) { return res; };
-	protected R clean(R res, Operation op) { return res; };
+	/**
+	 * Methode abstraite devant être implémentée par les classes filles,
+	 * pour tranformer une ressource vers son modèle de base de données
+	 * @param res
+	 * @return
+	 */
 	protected abstract T transform(R res);
+	
+	/**
+	 * Methode abstraite devant être implémentée par les classes filles,
+	 * pour tranformer une ressource vers son modèle de transfert
+	 * @param res
+	 * @return
+	 */
 	protected abstract R extract(T dbo);
+	
+	/**
+	 * Méthode abstraite devant être implémentée par les classes filles,
+	 * pour obtenir le dao propre à la ressource
+	 * @return
+	 */
+	protected abstract GenericDao<T> getDao(); 
+	
+	/**
+	 * Méthode abstraite devant être implémentée par les classes filles,
+	 * pour obtenir le validateur propre à la ressource
+	 * @return
+	 */
+	protected abstract Validator<R,T> getValidator();
+	
+	/**
+	 * Méthode mise à disposition des classes filles,
+	 * pour effectuer des actions entre la validation et la transformation vers le modèle de la base de donnée
+	 * @return
+	 */
+	protected R postValidationActions(R res, Operation op) { return res; };
+	
+	/**
+	 * Méthode mise à disposition des classes filles,
+	 * pour remplir des champs par défaut avant la validation 
+	 * @return
+	 */
+	protected R fillWithDefault(R res, Operation op) { return res; };
+	
+	/**
+	 * Méthode mise à disposition des classes filles,
+	 * pour remplir nettoyer des valeurs avant la validation 
+	 * @return
+	 */
+	protected R clean(R res, Operation op) { return res; };
 }
--- sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/Validator.java
+++ sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/Validator.java
@@ -8,28 +8,59 @@ import fr.metz.surfthevoid.tttt.rest.db.repo.GenericDao;
 import fr.metz.surfthevoid.tttt.rest.resources.ValidationException.Errors;
 import fr.metz.surfthevoid.tttt.rest.resources.ValidationException.Type;
 
+/**
+ * Classe abstraite générique de services pour la validation des ressources
+ * @author charles
+ *
+ * @param <R>
+ * @param <T>
+ */
+// Toutes les méthodes publiques appelées depuis un autre composant 
+// doivent être executées au sein d'un transaction 
 @Transactional(TxType.REQUIRED)
 public abstract class Validator<R extends Resource, T extends GenericDbo> {
 	
+	/**
+	 * Methode générique pour la validation d'une ressource 
+	 * @param input
+	 * @param op
+	 * @throws ValidationException
+	 */
 	public void validate(R input, Operation op) throws ValidationException {
 		Errors errors = new Errors();
+		// On vérifie que la ressource est présente
 		if(input == null){
 			throw new ValidationException(Type.INVALID_INPUT, errors);
 		}
+		// On vérifie que l'utilisateur a un droit d'accès à la ressource
 		if(!doUserCanAccessData(input, op, errors)){
 			throw new ValidationException(Type.INVALID_RIGHT, errors);
 		}
+		// On vérifie que l'opération effectuée correspond à l'état de la ressource
 		validateState(input, op, errors);
+		// On valide la valeur des données de la ressource
 		validateInput(input, op, errors);
+		// Si il y a des erreurs de validation on lance une exception
 		if(errors.hasErrors()){
 			throw new ValidationException(Type.INVALID_INPUT, errors);
 		}
 	}
 	
+	/**
+	 * Méthode générique servant à verifier la validité d'une opération,
+	 *  par rapport à l'état de la ressource 
+	 * @param input
+	 * @param op
+	 * @param errors
+	 * @throws ValidationException
+	 */
 	protected void validateState(R input, Operation op, Errors errors) throws ValidationException {
+		// Si la ressource à déjà un identifiant elle ne peut pas être créée
 		if(op == Operation.CREATE && input.getId() != null){
 			throw new ValidationException(Type.CONFLICT, errors);
-		} else if(op == Operation.UDPDATE){ 
+		} 
+		// Si la ressource doit être mise à jour elle doit avoir un identifiant référencé en base de donnée
+		else if(op == Operation.UDPDATE){ 
 			if(input.getId() == null){
 				throw new ValidationException(Type.INVALID_STATE, errors);
 			} 
@@ -40,18 +71,42 @@ public abstract class Validator<R extends Resource, T extends GenericDbo> {
 		}
 	}
 	
+	/**
+	 * Méthode générique de validation d'un identifiant
+	 * @param id
+	 * @throws ValidationException
+	 */
 	public void validateId(Long id) throws ValidationException {
 		Errors errors = new Errors();
+		// On vérifie que l'identifiant existe et correspond à une ressource en base de donnée
 		if(id == null || getDao().read(id) == null){
 			throw new ValidationException(Type.NO_CONTENT, errors);
 		}
+		// On vérifie que l'utilisateur à un droit d'accès à la ressource
 		if(!doUserCanAccessData(id, errors)){
 			throw new ValidationException(Type.INVALID_RIGHT, errors);
 		}
 	}
 	
+	/**
+	 * Méthode abstraite devant être implémentée par les classes filles,
+	 * pour valider que l'utilisateur à un droit d'accès en écriture à une ressource
+	 * @return
+	 */
 	protected Boolean doUserCanAccessData(R input, Operation op, Errors errors){return true;}
+	
+	/**
+	 * Méthode abstraite devant être implémentée par les classes filles,
+	 * pour valider que l'utilisateur à un droit d'accès en lecture à une ressource
+	 * @return
+	 */
 	protected Boolean doUserCanAccessData(Long id, Errors errors){return true;}
+	
+	/**
+	 * Méthode abstraite devant être implémentée par les classes filles,
+	 * pour certifier que les données d'une ressource sont valides 
+	 * @return
+	 */
 	protected abstract void validateInput(R input, Operation op, Errors errors) throws ValidationException;
 	protected abstract GenericDao<T> getDao();
 }

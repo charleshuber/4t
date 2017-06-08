e21de3ae400c4b907f0b3cb130ac30235152723a
charleshuber <charles.huber@hotmail.fr>
2017-06-08 06:05:17 +0200

    commentaires

--- sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/ResourceBoundary.java
+++ sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/resources/ResourceBoundary.java
@@ -17,35 +17,84 @@ import org.springframework.security.access.AccessDeniedException;
 import fr.metz.surfthevoid.tttt.rest.db.entity.GenericDbo;
 import fr.metz.surfthevoid.tttt.rest.resources.ValidationException.Type;
 
+/**
+ * Classe abstraite d'accès au webservice
+ * Elle concentre la totalité du code sur les accès au 4 methodes 
+ * principales POST, GET, PUT, DELETE
+ * @author charles
+ *
+ * @param <R>
+ */
+// Aucune transaction ne doit être commencée 
+// au moment de l'accès au webservice
 @Transactional(TxType.NEVER)
-public abstract class ResourceBoundary<R extends Resource>{
+public abstract class ResourceBoundary<R extends Resource> implements IResourceBoundary<R> {
 	
 	protected Log log = LogFactory.getLog(getClass());
-
+	
+	/**
+	 * Méthode générique de création d'une ressource
+	 */
+	@Override
 	public Response create(R resource) {
+		// Construction du code a appeler dans la méthode d'encapsulation
 		OperationalInterface<R> operation = input -> getStore().create(input);
+		// Appel de la méthode d'encapsulation en précisant,
+		// la fonction a exécuter et le code de retour HTTP en cas de succés
 		return doOperate(resource, Status.CREATED, operation);
 	}
 
+	/**
+	 * Méthode générique de lecture d'une ressource
+	 */
+	@Override
 	public Response read(Long id) {
+		// Construction du code a appeler dans la méthode d'encapsulation
 		OperationalInterface<Long> operation = input -> getStore().read(input);
+		// Appel de la méthode d'encapsulation en précisant,
+		// la fonction a exécuter et le code de retour HTTP en cas de succés
 		return doOperate(id, Status.OK, operation);
 	}
 	
+	/**
+	 * Méthode générique de mise à jour d'une ressource
+	 */
+	@Override
 	public Response update(R resource) {
+		// Construction du code a appeler dans la méthode d'encapsulation
 		OperationalInterface<R> operation = input -> getStore().update(input);
+		// Appel de la méthode d'encapsulation en précisant,
+		// la fonction a exécuter et le code de retour HTTP en cas de succés
 		return doOperate(resource, Status.OK, operation);
 	}
 	
+	/**
+	 * Méthode générique de suppression d'une ressource
+	 */
+	@Override
 	public Response delete(Long id) {
+		// Construction du code a appeler dans la méthode d'encapsulation
 		OperationalInterface<Long> operation = input -> getStore().delete(input);
+		// Appel de la méthode d'encapsulation en précisant,
+		// la fonction a exécuter et le code de retour HTTP en cas de succés
 		return doOperate(id, Status.OK, operation);
 	}
 	
+	/**
+	 * Méthode générique permettant de factoriser le traitement, 
+	 * dans le contexte d'un appel à l'une des 4 méthodes principales
+	 * @param input
+	 * @param successStatus
+	 * @param operation
+	 * @return
+	 */
 	public <T> Response doOperate(T input, Status successStatus, OperationalInterface<T> operation){
+		// Construction de l'objet permettant de construire la réponse HTTP
 		ResponseBuilder rb = new ResponseBuilderImpl();
 		try {
+			// Execution du code relatif à l'opération et récupération du résultat
 			Object entity = operation.operate(input);
+			// Définition du code de retour dans la réponse HTTP
 			rb.status(successStatus).entity(entity);
 		} catch (ValidationException e) {
 			handleValidationException(rb, e);
@@ -58,6 +107,14 @@ public abstract class ResourceBoundary<R extends Resource>{
 		return rb.build();
 	}
 	
+	/**
+	 * Méthode générique permettant de factoriser le traitement, 
+	 * dans le contexte d'un appel demandant une collection de ressources
+	 * @param input
+	 * @param successStatus
+	 * @param operation
+	 * @return
+	 */
 	public <L> Response readSet(ReadSetInterface<L> readCollectionInterface) {
 		ResponseBuilder rb = new ResponseBuilderImpl();
 		try {
@@ -74,6 +131,26 @@ public abstract class ResourceBoundary<R extends Resource>{
 		return rb.build();
 	}
 	
+	/**
+	 * Méthode générique permettant de factoriser le traitement ,
+	 * dans le contexte d'un appel HTTP de type GET devant effectuer une action en base de donnée
+	 * 
+	 * Dans le cadre d'une application stricte de la philosophie REST, cette méthode ne devrait pas exister,
+	 * mais le gain de temps est tel qu'il serait bête de s'en priver. 
+	 * 
+	 * Exemple:
+	 * Si une ressource "Groupe" contient une liste d'"Utilisateurs",
+	 * La principe du REST voudrait que pour ajouter ou supprimer un utilisateur du groupe,
+	 * on doive mettre en place une ressource de type "LienEntreUnGroupeEtUnUtilisateur" et utiliser une des méthodes vue précedemment.
+	 * 
+	 * Dans la pratique il est plus simple d'appeler une URL qui contient les identifiants du groupe et de l'utilisateur que l'on veut lier ou délier.
+	 * C'est ce à quoi sert cette fonction
+	 * 
+	 * @param input
+	 * @param successStatus
+	 * @param operation
+	 * @return
+	 */
 	public Response executePingAction(PingActionInterface pingActionInterface) {
 		ResponseBuilder rb = new ResponseBuilderImpl();
 		try {
@@ -90,7 +167,12 @@ public abstract class ResourceBoundary<R extends Resource>{
 		}
 		return rb.build();
 	}
-
+	
+	/**
+	 * 
+	 * @param rb
+	 * @param e
+	 */
 	protected void handleValidationException(ResponseBuilder rb, ValidationException e) {
 		if(e.getType() == Type.INVALID_RIGHT){
 			rb.status(Status.FORBIDDEN);

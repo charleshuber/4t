f9f089aa0731feea64451e967df4d89851aa0009
charleshuber <charles.huber@hotmail.fr>
2017-08-10 06:34:50 +0200

    working on cron

--- sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/time/TimeManager.java
+++ sources/business-rest/src/main/java/fr/metz/surfthevoid/tttt/rest/time/TimeManager.java
@@ -6,7 +6,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
